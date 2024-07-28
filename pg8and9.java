927d665d4243cde84509588abd4a0dd7c8b63ef2
Ansh23BAI1325 <ansh.v2023@vitstudent.ac.in>
2024-07-28 23:32:40 +0530

    Add files via upload

---
+++ pg8and9.java
@@ -0,0 +1,12 @@
+class pg8and9
+{
+    public static void main(String[] Args)
+    {
+        String firstname = "John";
+	    String lastname = "Doe";
+	    System.out.println(firstname+""+lastname);
+        System.out.println(firstname.concat(lastname));
+        }
+}
+        
+        
