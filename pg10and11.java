4112742284ba0cf8acaf77973b289dfcd54c19f8
Ansh23BAI1325 <ansh.v2023@vitstudent.ac.in>
2024-07-28 23:39:15 +0530

    Add files via upload

---
+++ pg10and11.java
@@ -0,0 +1,12 @@
+public class pg10and11 {
+    public class void main(String args[]){
+        String txt="We are so called \"Vikings\" from the north";
+        System.out.println(txt);
+        String x="10";
+        int y="20";
+        String z=x+y;
+        System.out.println(z);
+
+    }
+    
+}
