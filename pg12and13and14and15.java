6bab5d2160098c4779c4a60cf289625a20f6abdc
Ansh23BAI1325 <ansh.v2023@vitstudent.ac.in>
2024-07-28 23:48:52 +0530

    Add files via upload

---
+++ pg12and13and14and15.java
@@ -0,0 +1,10 @@
+public class pg12and13and14and15{
+
+public static void main(String[] args)
+{
+	System.out.println(Math.max(5,10));
+    System.out.println(Math.min(5,10));
+    System.out.println(Math.sqrt(81));
+    System.out.println(Math.abs(-5.9));
+}   
+}
