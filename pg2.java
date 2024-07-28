f06e76abbb8d99d3c4633ba01bcbc79b631a6fbd
Ansh23BAI1325 <ansh.v2023@vitstudent.ac.in>
2024-07-28 21:46:44 +0530

    Add files via upload

---
+++ pg2.java
@@ -0,0 +1,10 @@
+public class pg2{
+    public static void main(String[] args)
+     {
+     double mydouble=9.91d;
+     int myint=(int)mydouble;
+     System.out.println(myint);
+     System.out.println(mydouble);
+     }
+    }
+    
