c3f15269b5c1eb49b3ba7241c966d2bd8d3db466
Ansh23BAI1325 <ansh.v2023@vitstudent.ac.in>
2024-07-29 10:25:45 +0530

    Add files via upload

---
+++ pg16and17.java
@@ -0,0 +1,12 @@
+public class pg16 {
+    public static void main(String[] args) {  
+        int x = 10;  
+        int y = 18;  
+        System.out.println(x>y);  
+        System.out.println(x==y);
+        if(20>18) {  
+            System.out.println("20 is greater than 18");          
+    }    
+    } 
+    
+}
